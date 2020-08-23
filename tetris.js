3afadf82f59b38a6f32440ff65ab59f89e98c4ea
Susmith Raj Raj <srchiluka@gmail.com>
2020-08-23 16:03:35 +0000

    fixed problem that made only one piece fall into the matrix

--- tetris.js
+++ tetris.js
@@ -127,7 +127,7 @@ function playerMove(dir) {
 
 function playerReset() {
     const pieces = 'ILJOTSZ';
-    player.matrix = createPiece(pieces[pieces.length, pieces.width * Math.random() | 0]);
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
     player.pos.y = 0;
     player.pos.x = (arena[0].length / 2 | 0) -
                    (player.matrix[0].length / 2 | 0);
